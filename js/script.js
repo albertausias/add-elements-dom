0ed274e7b1ea6e6dbec9ebb353fbc43389e3e865
Tu Nombre <you@example.com>
2023-11-11 00:50:27 +0100

    changes exercice

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
  // Aquí tu código
+
+console.log('hola caracola');
+
+
+const agregar = document.getElementById('agregar');
+const lista = document.getElementById('lista');
+
+document.getElementById('agregar').addEventListener('click', function () {
+  let nuevoElemento = prompt('Aporta un nuevo elemento a nuestra lista:');
+
+  if (nuevoElemento) {
+    const lista = document.getElementById('lista');
+    elementoLista = document.createElement('ul');
+    elementoLista.textContent = nuevoElemento;
+    lista.appendChild(elementoLista);
+  }
+});
